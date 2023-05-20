5ac87b81527cef57c372d770b804ffb69345f98a
trace-martin <trace.martin7817@gmail.com>
2023-05-19 22:47:18 -0600

    cleaned a few items/updated readme

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
